41778cfe80c8375bccb5a69b860f3273d3aa7241
0xhelix <0xhelix@pm.me>
2022-07-02 12:15:47 +0100

    minor adjustments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Test ERC20 Token deployed on Goerli
+Test ERC20 Token deployed on Goerli
 
-# Token Address - 0x77145225112aad7f85385D0Fa5cff48079906886
+Token Address - 0x77145225112aad7f85385D0Fa5cff48079906886
 
-# FRFX
+FRFX
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     goerli: {
-      url: `${process.env.ALCHEMY_RINKEBY_URL}`,
-      accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`],
+      url: `${process.env.ALCHEMY_GOERLI_URL}`,
+      accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
     } 
   }
 };
